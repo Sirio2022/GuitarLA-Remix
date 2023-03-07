31fac40c8f2a5f400584a4f393fe3ad05d7798c9
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-03-07 08:27:07 -0500

    Nice

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import imagen from '../../public/img/nosotros.jpg';
+import styles from '~/styles/nosotros.css';
+
+export function meta() {
+  return {
+    title: 'GuitarLA - Sobre Nosotros',
+    description: 'Venta de guitarras, blog de música, cursos y más',
+  };
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles,
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as: 'image',
+    }
+  ];
+}
 
 function Nosotros() {
-  return <div>Alesixx esta loco!</div>;
+  return (
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className="contenido">
+        <img src={imagen} alt="imagen nosotros" />
+        <div>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text.
+          </p>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Nosotros;
